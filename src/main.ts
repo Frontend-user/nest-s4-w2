0249c2c755d2ab2292b2f7fa89703dda0464fac1
Robert <robersargsyan2023@gmail.com>
2024-03-03 19:08:30 +0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,55 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {HttpExceptionFilter} from "./exception.filter";
-import {ValidationPipe} from "@nestjs/common";
+import {BadRequestException, ValidationPipe} from "@nestjs/common";
 import {appSettings} from "./app.settings";
+import cookieParser from "cookie-parser";
+import {useContainer} from "class-validator";
 
 export async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-   appSettings(app)
+   // appSettings(app)
     // app.enableCors();
+    // app.use(cookieParser())
+    app.enableCors()
+    useContainer(app.select(AppModule), {fallbackOnErrors: true});
+    app.useGlobalPipes(new ValidationPipe(
+        {
 
+            transform: true,
+            stopAtFirstError: false,
+            exceptionFactory: (errors) => {
+                const errorsMessages: any = []
+                errors.forEach((e) => {
+                    if (e.constraints) {
+                        let s = Object.keys(e.constraints)
+                        s.forEach((key) => {
+                            if (e.constraints) {
+                                errorsMessages.push({
+                                    field: e.property,
+                                    message: e.constraints[key]
+                                })
+                            }
+                        })
+                    }
+                })
+                // errors.forEach((e) => {
+                //     const constraintKeys = Object.keys(e.constraints)
+                //     constraintKeys.forEach((ckey) => {
+                //         errorsMessages.push(
+                //             {
+                //                 message: e.constraints[ckey],
+                //                 field: e.property
+                //             }
+                //         )
+                //     })
+                //
+                // })
+                throw new BadRequestException({errorsMessages})
+            }
+
+        }
+    ))
     // app.useGlobalPipes(new ValidationPipe())
     // app.useGlobalFilters( new HttpExceptionFilter());
     await app.listen(3000);
